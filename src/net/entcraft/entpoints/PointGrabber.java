479e538b6ec5709806b71b4218be08315bf3ca18
AlexDGr8r <alextrisner@gmail.com>
2012-09-27 11:04:34 -0500

    Made PointGrabber future-proof with enum for columns

--- src/net/entcraft/entpoints/PointGrabber.java
+++ src/net/entcraft/entpoints/PointGrabber.java
@@ -22,7 +22,7 @@ public class PointGrabber {
 		int totalPoints = 0;
 		try {
 			rs.first();
-			totalPoints = rs.getInt("donatedPoints") + rs.getInt("earnedPoints") + rs.getInt("vouchedPoints");
+			totalPoints = rs.getInt(PointColumn.Donated.toString()) + rs.getInt(PointColumn.Earned.toString()) + rs.getInt(PointColumn.Vouched.toString());
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -30,23 +30,23 @@ public class PointGrabber {
 	}
 	
 	public int getDonatedPoints(String player) {
-		return getIntValue(player, "donatedPoints");
+		return getIntValue(player, PointColumn.Donated);
 	}
 	
 	public int getEarnedPoints(String player) {
-		return getIntValue(player, "earnedPoints");
+		return getIntValue(player, PointColumn.Earned);
 	}
 	
 	public int getVouchedPoints(String player) {
-		return getIntValue(player, "vouchedPoints");
+		return getIntValue(player, PointColumn.Vouched);
 	}
 	
-	private int getIntValue(String player, String column) {
-		ResultSet rs = sql.sqlQuery("SELECT " + column + " FROM " + Main.tableName + " WHERE pname='" + player + "';");
+	private int getIntValue(String player, PointColumn column) {
+		ResultSet rs = sql.sqlQuery("SELECT " + column.toString() + " FROM " + Main.tableName + " WHERE pname='" + player + "';");
 		int p = 0;
 		try {
 			rs.first();
-			p = rs.getInt(column);
+			p = rs.getInt(column.toString());
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -54,20 +54,39 @@ public class PointGrabber {
 	}
 	
 	public void addDonatedPoints(String player, int amount) {
-		addPoints(player, amount, "donatedPoints");
+		addPoints(player, amount, PointColumn.Donated);
 	}
 	
 	public void addEarnedPoints(String player, int amount) {
-		addPoints(player, amount, "earnedPoints");
+		addPoints(player, amount, PointColumn.Earned);
 	}
 	
 	public void addVouchedPoints(String player, int amount) {
-		addPoints(player, amount, "vouchedPoints");
+		addPoints(player, amount, PointColumn.Vouched);
 	}
 	
-	private void addPoints(String player, int amount, String column) {
+	private void addPoints(String player, int amount, PointColumn column) {
 		int oldPoints = getIntValue(player, column);
-		sql.standardQuery("UPDATE " + Main.tableName + " SET " + column + "=" + (oldPoints + amount) + " WHERE pname='" + player + "';");
+		sql.standardQuery("UPDATE " + Main.tableName + " SET " + column.toString() + "=" + (oldPoints + amount) + " WHERE pname='" + player + "';");
+	}
+	
+	private enum PointColumn {
+		
+		Donated("donatedPoints"),
+		Earned("earnedPoints"),
+		Vouched("vouchedPoints");
+		
+		private String columnName;
+		
+		PointColumn(String s) {
+			columnName = s;
+		}
+		
+		@Override
+		public String toString() {
+			return columnName;
+		}
+		
 	}
 	
 }
